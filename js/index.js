f6dae0bc8e7e1c9f309cda19a5919db6b183eef4
CODEVDR <96605996+CODEVDR@users.noreply.github.com>
2023-06-03 17:29:03 +0530

    main commit

---
+++ js/index.js
@@ -0,0 +1,65 @@
+// adding title
+document.title = "Untitled - Notepad";
+// styling textarea
+let textarea = document.getElementById("notepad");
+if (screen.width < 900) {
+  textarea.style.width = `${screen.width}px`;
+  document.getElementById("head").innerHTML = null;
+} else {
+  textarea.style.width = `${screen.width - 50}px`;
+}
+textarea.style.height = `${screen.height}px`;
+// Main
+// Saving File in LocalStorage
+document.getElementById("save").onclick = () => {
+  let fileName = prompt("Enter FileName To Save : ").toLowerCase();
+  let content = document.getElementById("notepad").value;
+  if (fileName === "") {
+    alert("Error !! Filename Can't Be Empty");
+  } else if (content === "") {
+    alert("Error !! File Can't Be Saved With Empty Content");
+  } else {
+    document.title = `${fileName} - Notepad`;
+    localStorage.setItem(fileName, content);
+    alert(`${fileName} Stored Sucessfully!!!!`);
+  }
+};
+// Saving As File in LocalStorage
+document.getElementById("saveAs").onclick = () => {
+  let fileName = prompt("Enter FileName To SaveAs : ").toLowerCase();
+  let content = document.getElementById("notepad").value;
+  if (fileName === "") {
+    alert("Error !! Filename Can't Be Empty");
+  } else if (content === "") {
+    alert("Error !! File Can't Be Saved With Empty Content");
+  } else {
+    document.title = `${fileName} - Notepad`;
+    localStorage.setItem(fileName, content);
+    alert(`${fileName} Stored Sucessfully!!!!`);
+  }
+};
+// opening File From LocalStorage
+document.getElementById("open").onclick = () => {
+  let files = [];
+  for (var i = 0, len = localStorage.length; i < len; ++i) {
+    files.push(localStorage.key(i));
+  }
+
+  let fileName = prompt(
+    `Enter FileName To Open\nFiles Available : ${files}`
+  ).toLowerCase();
+  let val = localStorage.getItem(fileName);
+  if (val === null) {
+    alert("No Such File In Our Database");
+  } else {
+    document.getElementById("notepad").innerHTML = val;
+    document.title = `${fileName} - Notepad`;
+  }
+};
+document.getElementById("close").onclick = () => {
+  let val = confirm("Do You Want To Close File??");
+  if (val) {
+    document.title = `Untitled - Notepad`;
+    document.getElementById("notepad").innerHTML = null;
+  }
+};
